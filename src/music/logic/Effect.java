0468feef0bb467da6ccbf1b17377cfc96f0a98ff
David Rasp <david.j.rasp@gmail.com>
2015-04-04 21:38:04 +0200

    some logic

--- src/music/logic/Effect.java
+++ src/music/logic/Effect.java
@@ -1,5 +1,7 @@
 package music.logic;
 
 public class Effect {
+	char value;
+	int paramter;
 
 }
--- src/music/logic/Note.java
+++ src/music/logic/Note.java
@@ -1,5 +1,19 @@
 package music.logic;
 
 public class Note {
+	char pitch;
+	int octave;
+	boolean halftone;
+	@Override
+	public String toString() {
+		if(this.halftone)
+		{
+			return pitch + " # " + octave;
+		}
+		else
+		{
+			return pitch + " - " + octave;
+		}
+	}
 
 }
