767103d4b3ceec63b1a8295a29e7d1c4f2419d16
David Rasp <david.j.rasp@gmail.com>
2015-04-08 18:28:12 +0200

    Implementation of some toString-methods

--- src/music/logic/Pattern.java
+++ src/music/logic/Pattern.java
@@ -8,4 +8,27 @@ public class Pattern {
 	 */
 	Column[] columns;
 
+	@Override
+	public String toString() {
+		StringBuilder patternString = new StringBuilder();
+		for(int rowNr = 0; rowNr < this.columns[0].length ; rowNr++)
+		{
+			patternString.append(String.format("ROW %02X", rowNr));
+			for(int colNr = 0; colNr < this.columns.length; colNr++)
+			{
+				if(this.columns[colNr] instanceof InstrumentColumn)
+				{
+					patternString.append(" :" + ((InstrumentColumn) this.columns[colNr]).ticks[rowNr].toString());
+				}
+				else if(this.columns[colNr] instanceof NoiseColumn)
+				{
+					//Noise Columns todo
+				}
+			}
+		}
+		return patternString.toString();
+		
+		
+	}
+
 }
