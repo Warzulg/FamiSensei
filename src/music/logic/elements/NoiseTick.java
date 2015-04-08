be2b31fc5bf9d0dbbbf61448b774d1f8e12bb116
David Rasp <david.j.rasp@gmail.com>
2015-04-09 01:11:03 +0200

    Refactoring and implementation of Noise

--- src/music/logic/elements/NoiseTick.java
+++ src/music/logic/elements/NoiseTick.java
@@ -1,5 +1,14 @@
 package music.logic.elements;
 
-public class NoiseTick {
+public class NoiseTick extends Tick{
 
+	private Noise noise;
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
 }
