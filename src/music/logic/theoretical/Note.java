2bea7056af75b40a2183568540f3ee80399c4f6c
Warzulg <bartuk@hotmail.de>
2015-09-11 18:00:57 +0200

    few randomizing/generating methods and fixes

--- src/music/logic/theoretical/Note.java
+++ src/music/logic/theoretical/Note.java
@@ -23,15 +23,27 @@ public class Note {
 	
 	// SETTERS
 	public void setPitch(int n) {
-		pitch = n;
+		pitch = Math.abs(n);
 	}
 	
 	public boolean isSharp() {
 		return pitch % 2 == 1;
 	}
 	
+	/**
+	 * adds or subtracts steps from the pitch of a note.
+	 * 1 step = half step; 2 steps = full step; 12 steps = full octave
+	 * @param n number of steps to go up (positive value) or down (negative value).
+	 */
 	public void step(int n) {
 		pitch += n;
+		pitch = Math.abs(pitch);
+	}
+	
+	public String toString() {
+		
+		return String.format("(02%d)", pitch);
+		
 	}
 	
 	
